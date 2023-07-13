1148c92aff98359bdd907685baa5cd1085516c66
InvincibleCookie <57621103+InvincibleCookie@users.noreply.github.com>
2023-07-13 20:57:41 +0700

    Add level10

---
+++ Top down game/Assets/Level10/stopwatch.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using TMPro;
+
+public class Stopwatch : MonoBehaviour
+{
+    public TextMeshPro timerText;
+    public float targetTime;
+    private float elapsedTime;
+    private bool isRunning;
+    private EnemyCollis2[] enemies;
+
+    private void Start()
+    {
+        isRunning = true;
+        enemies = FindObjectsOfType<EnemyCollis2>();
+    }
+
+    private void Update()
+    {
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimerUI();
+
+            if (elapsedTime >= targetTime)
+            {
+                ActivateActions();
+            }
+        }
+    }
+
+    private void UpdateTimerUI()
+    {
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+        int milliseconds = Mathf.FloorToInt((elapsedTime * 1000f) % 1000f);
+
+        timerText.text = string.Format("{0:00}:{1:000}", seconds, milliseconds);
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        elapsedTime = 0f;
+        UpdateTimerUI();
+    }
+
+    private void ActivateActions()
+    {
+        // ¬ыполн€ем действи€ при достижении времени
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            player.transform.position = new Vector2(-10f, 0f);
+        }
+
+        foreach (EnemyCollis2 enemy in enemies)
+        {
+            enemy.ResetEnemiesPosition();
+        }
+
+        ResetTimer();
+    }
+}
--- Top down game/Assets/Level9/EnemyCollision2.cs
+++ Top down game/Assets/Level9/EnemyCollision2.cs
@@ -4,11 +4,13 @@ public class EnemyCollis2 : MonoBehaviour
 {
     private Vector2 enemyStartPosition;
     private realTimeTimer RealtimerController;
+    private Stopwatch stopwatch;
 
     private void Start()
     {
         enemyStartPosition = transform.position;
         RealtimerController = FindObjectOfType<realTimeTimer>();
+        stopwatch = FindObjectOfType<Stopwatch>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -18,10 +20,14 @@ public class EnemyCollis2 : MonoBehaviour
             collision.transform.position = new Vector2(-10f, 0f);
             ResetEnemiesPosition();
             ResetTimer();
+            if (stopwatch != null)
+            {
+                stopwatch.ResetTimer();
+            }
         }
     }
 
-    private void ResetEnemiesPosition()
+    public void ResetEnemiesPosition()
     {
         EnemyCollis2[] enemies = FindObjectsOfType<EnemyCollis2>();
         foreach (EnemyCollis2 enemy in enemies)
